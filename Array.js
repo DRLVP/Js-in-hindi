e71e1bac597855a173f79a2b08848ee4f2d4e00d
DRLOV <phukondurlov45@gmail.com>
2023-08-21 09:07:36 +0530

    add new array, callback, forEach, and index file

---
+++ Array.js
@@ -0,0 +1,13 @@
+// const myArr = [2, 5, 6, 12, 25];
+// // console.log(typeof myArr, myArr);
+// // console.log(myArr.slice(-1));
+// // console.log(myArr);
+// // console.log(myArr.splice(1, 3));
+// // console.log(myArr);
+
+// const arr = new Array('god', 'oh', 'my');
+// const newArr = arr.concat(myArr);
+// console.log(newArr);
+
+
+
