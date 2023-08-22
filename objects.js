dd4ff99853338ecf10d4a886ed6b4ca1328537e0
DRLOV <phukondurlov45@gmail.com>
2023-08-22 08:59:09 +0530

    added new objects file

--- objects.js
+++ objects.js
@@ -1,11 +1,38 @@
 // OBJECTS IN JAVASCRIPT?
-
+/*
+const mySym = Symbol('symbol1');
 const obj = {
-    name:"Durlov",
-    age:22,
-    skills:function skills() { //object or vitort thoka function ok method buli kuwa hoi
-        alert("HTML, CSS, JAVASCRIPT");
+    'full name': "Durlov",
+    age: 22,
+    [mySym]: "mykey",
+    skills: function skills() { //object or vitort thoka function ok method buli kuwa hoi
+
     }
 }
+*/
 
-console.log(obj.skills());
+let user = {
+    id: 785688,
+    firstName: "John",
+    lastName: 'Doe',
+    email: 'demo@google.com',
+    address: {
+        street: '3400 NW 9th Ave.',
+        city: "Seattle",
+        state: "WA",
+        zipcode: 98101
+    },
+}
+
+user.greet = function () {
+    console.log(`have a good day ${this.firstName} ${this.lastName}`);
+}
+// console.log(`Hello  ${user.firstName} ${user.lastName}`);
+
+// console.log(user.greet());
+
+
+let city = {
+    countryName: 'USA',
+    city: 'new york',
+}
