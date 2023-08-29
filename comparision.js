bcaa3c232b793d40103549f4b74df5699670f409
DRLOV <phukondurlov45@gmail.com>
2023-08-29 11:10:37 +0530

    added comparision file

---
+++ comparision.js
@@ -0,0 +1,19 @@
+/*
+console.log(5 > 2);
+console.log(5 >= 2);
+console.log(1 < 2);
+console.log(2 <= 2);
+
+
+console.log("55" > 22);
+console.log("11" == 11);
+console.log("11" === 11);
+console.log(null === 0); //This method called strict check
+console.log(null >= 0);
+console.log(null < 0);
+console.log(null <= 0);
+
+*/
+// AVOID THESE TYPE OF COMPARISION
+// console.log(NaN > 0);
+// console.log("11" >= 10);
---
+++ operations.js
@@ -0,0 +1,25 @@
+// OPERATIONS IN JAVASCRIPT
+
+let num = 5;
+let negNum = Number(-num);
+// console.log(negNum);
+
+// console.log(2 ** 3);
+
+// MOST IMPORTANT IS MODULOUS OPERATOR
+
+// console.log(2 % 1);
+
+// console.log(typeof ("1" + 2 + 3));
+// console.log(typeof (3 + 2 + "3"));
+
+// console.log(typeof ("1" + 2 + 3));
+// console.log(typeof (3 + 2 + "3")); //DON'T USE THESE TYPE OF CODE
+
+// console.table([+true, +false]);
+
+let gameCounter = 100;
+++gameCounter
+console.log(gameCounter);
+
+// PREFIX AND POSTFIX OPERATOR
