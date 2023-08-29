42da1877132cc66b71ded712919672cc7a31728f
DRLOV <phukondurlov45@gmail.com>
2023-08-29 09:02:44 +0530

    conversitonOfDataTypes

---
+++ dataTypeConversion.js
@@ -0,0 +1,25 @@
+let age = "22abs"; // IYAT GUARANTEE NAI JE AI2 VALUE NUMBERAI HBO
+
+// console.log(typeof age);
+// console.log(typeof (age));
+/*
+let myAge = Number(age);
+console.log(typeof myAge); //IYAT GUARANTEE ASE JE EE NUMBERAI HBO
+
+console.log(myAge); //IYAT VALUE NaN AHIBO
+*/
+
+let name = true;
+let newNum = String(name);
+// console.log(typeof newNum);
+// console.log(newNum);
+
+/*=============IMPORTANT===========*/
+
+// JAVASCRIPT OT NaN OR TYPE U HOISE NUMBER 
+
+// let single = "hoi be";
+let single = document.all;
+let hosa = Boolean(single);
+// console.log(typeof hosa);
+// console.log(hosa);
--- dataTypes.js
+++ dataTypes.js
@@ -1,6 +1,21 @@
-// DATATYPES IN JAVASCRIPT
 
 "use strict";  //ai2 likhile javascripte jiman code ase soboke notun code buli dhore
 
 // CODING BA PROGRAMMINGOT READABILITY HOKOLUT KOI IMPORTANT TOPIC HOI
 
+
+// PRIMITVE DATATYPES IN JAVASCRIPT
+
+
+/* ========================================================
+    number, null, string, symbol, boolean, bigInt, undefined
+==========================================================*/
+
+// NULL javascriptot ata standalone value hoi, jodi ami null assign koru tar mane hoise khali ba tat aku nai;
+
+// KUNU ATA COMPONET UNIQUE BUJABOLE AMI SYMBOL USE KORU
+
+// JAVASCRIPOT NULL ATA OBJECT HOI
+
+let num = 22
+name = "drlov" 
