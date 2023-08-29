80f869ee1c49ad1ae43bdb51ac57ff7da12828b6
DRLOV <phukondurlov45@gmail.com>
2023-08-29 08:22:56 +0530

    adding some basic file in js

---
+++ dataTypes.js
@@ -0,0 +1,6 @@
+// DATATYPES IN JAVASCRIPT
+
+"use strict";  //ai2 likhile javascripte jiman code ase soboke notun code buli dhore
+
+// CODING BA PROGRAMMINGOT READABILITY HOKOLUT KOI IMPORTANT TOPIC HOI
+
---
+++ variables.js
@@ -0,0 +1,19 @@
+// declare const
+
+const myNum = 45;
+// myNum = 22;  // you cannot change constant
+// console.log(myNum);
+
+let city = "charaideo";
+city = "lakwa";
+
+var pincode = 785699;
+pincode = 255689;
+let state;
+// console.table([myNum, city, pincode, state]);
+
+/* 
+```   prefer not to use var, because of issue in block scope and functional scope ```
+
+*/
+
