9ebb84e88b3d367188c5a69a9542cb2ee6d6819d
DRLOV <phukondurlov45@gmail.com>
2023-08-31 08:27:14 +0530

    array prctice question

--- arrays.js
+++ arrays.js
@@ -31,7 +31,7 @@ const dc = ['superman', 'batman', 'flash'];
 // const superHero = marvel.concat(dc); //return a new array
 // console.log(superHero);
 
-const superHero = [...marvel, ...dc];
+// const superHero = [...marvel, ...dc];
 // console.log(superHero);
 
 const num = [1, 2, 3, [4, 5, 6, [7, 8, 9, [10, 11, 12, [13, 14, 15]]]]];
@@ -45,3 +45,48 @@ const movie = [{ name: 'thor', collection: 2.5 }, { name: 'iron-man', collection
 // console.log(movie[0].collection);
 // console.log(movie[0].name);
 
+let name = 'durlov',
+    title = 'phukon',
+    address = 'lakwa',
+    pin = 785688;
+// console.log(Array.from(name));
+const user = Array.of(name, title, address, pin);
+// user.forEach(() => {
+//     console.log(user.toString());
+// })
+
+
+
+/* ================== ARRAY PROBLEM SOLVE ================ */
+// Problem 1: Remove Duplicates from Sorted Array
+
+const arr = [1, 1, 1, 3, 4, 6, 78, 4, 4, 4, 3, 3, 6, 6];
+
+const newArr = arr.filter(function (elem, index, arr) {
+    // console.log(elem);
+    // console.log(index);
+    // console.log(arr);
+    return index === arr.indexOf(elem);
+});
+// console.log(newArr);
+
+const marvelHero = ['iron-man', 'thor', 'spiderman', 'hulk'];
+const allHero = ['deadpool', 'spiderman', 'thor', 'flash', 'batman', 'hulk'];
+
+function superHero() {
+    const superHeros = [...marvelHero, ...allHero];
+    // console.log(superHeros);
+    const heros = superHeros.filter(function (elem, i, hero) {
+        if (i === hero.indexOf(elem)) {
+            return i;
+        }
+    });
+    console.log(heros);
+};
+
+// superHero();
+
+// Problem 2: Product of Array Except Self
+
+const myArr = [1, 2, 3, 4];
+// console.log(myArr);
