a3c09c4e04a15a9042eeec34532ecabcf4a0a133
DRLOV <phukondurlov45@gmail.com>
2023-09-02 10:04:20 +0530

    modify local & global scope

--- local&global-scope.js
+++ local&global-scope.js
@@ -1,3 +1,20 @@
 // global scope ot bonuwa variable jodi var keyword ere bonuwa hoi tenete ee local scopotu ake thakibo, haikarone var use kora tu avoid koribo lage 
 
 // global scopot bonuwa variable block scope ba local scope use koribo pare kin2 block scopor ba local scopor vitorot thoka variable global scope access koribo nuware
+
+
+
+
+function one() {
+    const username = "durlov";
+
+    function two() {
+        const website = "youtube";
+        console.log(username);
+        console.log(website);
+    }
+    // console.log(website); //ee errpr dibo karon childrenor pora ami bos2 lobo nuwaru
+
+    two();
+}
+one();
