7d24b12dc416e823e21761393ad7c0efe1444cec
DRLOV <phukondurlov45@gmail.com>
2023-09-06 07:40:11 +0530

    added controlFlow & loops files

---
+++ controlFlow.js
@@ -0,0 +1,6 @@
+//IF STATEMENT
+
+if (true) {
+    console.log('hello hello');
+}
+
---
+++ loops.js
@@ -0,0 +1,38 @@
+// javaScripot loops ok iteration buli kuwa hoi
+
+const users = [{ name: 'drlv', id: 11111 }, { name: 'jhon', id: 22222 }];
+
+// for (let i = 0; i < users.length; i++) {
+//     const element = users[i];
+//     console.log(element);
+// }
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(`outerloop : ${i}`);
+//     for (let j = 0; j < 5; j++) {
+//         // const element = array[j];
+//         //console.log(`innerloop : ${j}, and outer loop ${i}`);
+//         console.log(i + '*' + j + ' = ' + i * j);
+//     }
+
+// }
+
+const arr = ['thor', 'ironman', 'hulk'];
+
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    // console.log(element);
+}
+
+// BREAK AND CONTINUE
+
+for (let i = 1; i <= 20; i++) {
+    const element = i;
+    if (i === 5) {
+        console.log('deteced 5');
+        // break;
+        continue;
+    }
+
+    // console.log(element);
+}
