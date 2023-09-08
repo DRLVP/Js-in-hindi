287f8f4e606ddc0064547a4a633beb6abe1745e4
DRLOV <phukondurlov45@gmail.com>
2023-09-08 08:54:13 +0530

    added DOM files

---
+++ DOM.js
@@ -0,0 +1,9 @@
+const body = document.getElementById('body');
+body.style.backgroundColor = '#212121';
+body.style.color = '#fff';
+
+const list = document.querySelectorAll('p');
+console.log(list);
+
+list[0].style.backgroundColor = 'coral';
+list[0].style.padding = '20px';
