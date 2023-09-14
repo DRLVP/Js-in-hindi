adc733dd3a13f6939f07b64601817eff31e81eb0
DRLOV <phukondurlov45@gmail.com>
2023-09-14 07:37:43 +0530

    learn fetch in depth

---
+++ fetch.js
@@ -0,0 +1,11 @@
+// FETCH IN JAVASCRIPT :-- fetch ata network based request hoi jit2 naki 1 feb 2022 t node.js runtime or logot merge kora hoi aaru eee promise return kore
+
+// interview question : jodi fetch or joriote ami nwtwork request koru , tar pasot jodi response 404 error ahe tente resolve() state ot ahibo ne reject state ot ahibo / ANSWER:- resolve state ot ahibo jodi network request complete nohoi tetia ha error ahibo
+
+// fetch or karone high priority micro-task queue
+
+// fetche duta bhgot kam kore prothom bhg tu jai web browser api ba node o loi network request or babe aru 2nd bhag tu jai memory reserve koriboloi
+
+// memoryt tinita bos2 take ata variable aru 2ta array onfulfiled [] resolve or babe ru onrejection[] reject or babe aru ai kaita hoise private field iyak ami access koribo nuwaru
+
+// jetia fetch web api ba node api loi network request koriboloi jai teia hoi2 request tu fullfil hbo nohole reject hbo jodi request fullfil hoi tatia fullfil response on fulfiled array loi jbo jot ata function ase{include 404} aru ai function 2 a response private variable ot  add kore aru ai private variabole tu a data khini ami bonuwa variable tut inject koribo ji2 naki global variabolot ase / reject holeu ake process follow hbo
